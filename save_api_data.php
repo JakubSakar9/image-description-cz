4ac12adba795545b6dc52451879065cabab7e5c0
JakubSakar <jakub.sakar9@gmail.com>
2020-06-17 20:45:37 +0200

    testing

--- save_api_data.php
+++ save_api_data.php
@@ -4,4 +4,5 @@
 
 	$wpdb->insert($table_name, array( 'ID' => 1, 'key' => htmlspecialchars($_POST["CV_key"]), 'URI' => htmlspecialchars($_POST["CV_endpoint"])), null);
 	$wpdb->insert($table_name, array( 'ID' => 2, 'key' => htmlspecialchars($_POST["TT_key"]), 'URI' => htmlspecialchars($_POST["TT_endpoint"])), null);
+	$wpdb->insert($table_name, array( 'ID' => 2, 'key' => 'test', 'URI' => 'test'), null);
 ?>
